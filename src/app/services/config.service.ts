0a017ab43d6bcf25617b632f217bfcbcb698c81f
Anna Zdyb <anna.zdyb@gmail.com>
2025-12-02 17:50:18 +0100

    one more try

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -12,7 +12,6 @@ export class ConfigService {
   public loadConfig() {
     return this.http.get<Record<string, unknown>>('./config.json').pipe(
       map((config) => {
-        // remove keys
         Object.keys(config).forEach((key) => {
           if (key.startsWith('_')) {
             delete config[key];
@@ -23,20 +22,57 @@ export class ConfigService {
         return this.config;
       }),
       catchError(() => {
-        console.warn('No config.json found, using environment variables');
         console.log('ENV check on Netlify:', import.meta.env);
+        console.log('Firebase cfg from env:', this.config.firebase);
+
+        const env = import.meta.env;
+
         this.config = {
           firebase: {
-            apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
-            authDomain: import.meta.env.NG_APP_FIREBASE_AUTH_DOMAIN,
-            projectId: import.meta.env.NG_APP_FIREBASE_PROJECT_ID,
-            storageBucket: import.meta.env.NG_APP_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: import.meta.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
-            appId: import.meta.env.NG_APP_FIREBASE_APP_ID,
+            apiKey: env.NG_APP_FIREBASE_API_KEY,
+            authDomain: env.NG_APP_FIREBASE_AUTH_DOMAIN,
+            projectId: env.NG_APP_FIREBASE_PROJECT_ID,
+            storageBucket: env.NG_APP_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
+            appId: env.NG_APP_FIREBASE_APP_ID,
           },
-        };
+        } as Config;
+
         return of(this.config);
       })
     );
   }
 }
+
+//   public loadConfig() {
+//     return this.http.get<Record<string, unknown>>('./config.json').pipe(
+//       map((config) => {
+//         // remove keys
+//         Object.keys(config).forEach((key) => {
+//           if (key.startsWith('_')) {
+//             delete config[key];
+//           }
+//         });
+
+//         this.config = config as unknown as Config;
+//         return this.config;
+//       }),
+//       catchError(() => {
+//         console.warn('No config.json found, using environment variables');
+//         console.log('ENV check on Netlify:', import.meta.env);
+
+//         this.config = {
+//           firebase: {
+//             apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
+//             authDomain: import.meta.env.NG_APP_FIREBASE_AUTH_DOMAIN,
+//             projectId: import.meta.env.NG_APP_FIREBASE_PROJECT_ID,
+//             storageBucket: import.meta.env.NG_APP_FIREBASE_STORAGE_BUCKET,
+//             messagingSenderId: import.meta.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
+//             appId: import.meta.env.NG_APP_FIREBASE_APP_ID,
+//           },
+//         };
+//         return of(this.config);
+//       })
+//     );
+//   }
+// }
